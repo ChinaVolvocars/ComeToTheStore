89e07e9d069c9ca7bae31d3872371d1b33c55895
Tiger <atlantisspeed@gmail.com>
2019-09-15 20:42:05 +0800

    Initial commit

---
+++ app/src/main/java/com/tiger/come/store/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.tiger.come.store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    tvLocation.setOnClickListener {
+      println("22222")
+    }
+
+    tvSearch.setOnClickListener {
+      println("搜索")
+    }
+  }
+}
